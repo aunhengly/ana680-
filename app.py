3618663828f0dd7ce2bed56b672fccdff23fc1a2
Hengly Aun <aunhengly@gmail.com>
2023-11-06 16:10:22 -0800

    Edit the index.html file to keep the recording of the prediction

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
-# import joblib
+
 app = Flask(__name__)
 filename = 'file_StudentsPerformance.pkl'
-model = pickle.load(open(filename, 'rb'))    # load the model
+model = pickle.load(open(filename, 'rb'))
 
+# Initialize an empty list to store predictions
+predictions = []
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', predictions=predictions)
 
-
-@app.route('/predict', methods=['POST'])  # The user input is processed here
+@app.route('/predict', methods=['POST'])
 def predict():
     math_score = request.form['math_score']
     reading_score = request.form['reading_score']
     writing_score = request.form['writing_score']
-    pred = model.predict(
-        np.array([[math_score,reading_score,writing_score,]]))
-    # print(pred)
-    return render_template('index.html', predict=str(pred))
-
+    
+    pred = model.predict(np.array([[math_score, reading_score, writing_score,]]))
+    result = f"Math Score: {math_score}, Reading Score: {reading_score}, Writing Score: {writing_score} => Prediction: {pred[0]}"
+    
+    # Append the result to the list of predictions
+    predictions.append(result)
+    
+    # Display the results and clear the form
+    return render_template('index.html', predict=pred[0], predictions=predictions)
 
 if __name__ == '__main__':
     app.run(debug=True)
